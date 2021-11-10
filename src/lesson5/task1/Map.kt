c28dfd15f9420c26e1ae5371068978ecd8909c81
milen2704 <71918774+milen2704@users.noreply.github.com>
2021-11-10 15:34:46 +0100

    Milen Nikolov lesson 5 fun findSumOfTwo

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -312,7 +312,15 @@ fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<Stri
  *   findSumOfTwo(listOf(1, 2, 3), 4) -> Pair(0, 2)
  *   findSumOfTwo(listOf(1, 2, 3), 6) -> Pair(-1, -1)
  */
-fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> = TODO()
+fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
+    val mapIndex = mutableMapOf<Int, Int>()
+    for (i in list.indices) {
+        if (list[i] in mapIndex) return Pair(mapIndex.getOrDefault(list[i], -1), i)
+        mapIndex[number - list[i]] = i
+    }
+    return Pair(-1, -1)
+}
+
 
 /**
  * Очень сложная (8 баллов)
